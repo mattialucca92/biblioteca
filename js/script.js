5b1f25807a3a109634f6d1a3a9f21f0d48f2e49f
Mattia Lucca <mattialucca92@gmail.com>
2025-02-26 11:15:16 +0100

    scrittura codice js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
-  console.log("js collegato");
+  $("#btn-categoria").click(function () {
+    let categoria = $("#categoria").val().trim();
+    if (categoria === "") {
+      alert("Compila il campo categoria");
+      return;
+    }
+    let pathJson = `https://openlibrary.org/subjects/${categoria}.json`;
+
+    $.ajax({
+      url: pathJson,
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        $("#risultati").empty();
+
+        data.works.forEach((libro) => {
+          // console.log(libro)
+          let titolo = libro.title;
+          let autore = libro.authors
+            ? libro.authors.map((n) => n.name).join("  -  ")
+            : "Autore non disponibile";
+          $("#risultati").append(`
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${titolo}</h5>
+              <p class="card-text">${autore}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          `);
+        });
+        console.log("data", data);
+      },
+      error: function (error) {
+        console.error("Errore durante la richiesta:", error);
+      },
+    });
+  });
 });
