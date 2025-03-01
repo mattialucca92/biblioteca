7ada04778538833f7d38e56fa74cf507379c06c0
Mattia Lucca <mattialucca92@gmail.com>
2025-03-01 12:04:01 +0100

    completamento progetto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("#btn-categoria").click(function () {
-    let categoria = $("#categoria").val().trim();
+    let categoria = $("#categoria").val().trim().toLowerCase();
     if (categoria === "") {
       alert("Compila il campo categoria");
       return;
@@ -15,22 +15,44 @@ $(document).ready(function () {
         $("#risultati").empty();
 
         data.works.forEach((libro) => {
-          // console.log(libro)
           let titolo = libro.title;
           let autore = libro.authors
-            ? libro.authors.map((n) => n.name).join("  -  ")
+            ? libro.authors.map((n) => n.name).join(" - ")
             : "Autore non disponibile";
+          let key = libro.key;
+
           $("#risultati").append(`
-            <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">${titolo}</h5>
-              <p class="card-text">${autore}</p>
-              <a href="#" class="btn btn-primary">Clicca per vedere dettaglio</a>
-            </div>
-          </div>
-          `);
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${titolo}</h5>
+        <p class="card-text">${autore}</p>
+        <button class="btn btn-primary btn-dettaglio" data-key="${key}" data-titolo="${titolo}" data-bs-toggle="modal" data-bs-target="#desc-modal">
+          Clicca per vedere dettaglio
+        </button>
+      </div>
+    </div>
+  `);
         });
-        console.log("data", data);
+      },
+      error: function (error) {
+        console.error("Errore durante la richiesta:", error);
+      },
+    });
+  });
+
+  $(document).on("click", ".btn-dettaglio", function () {
+    let key = $(this).data("key");
+    let titoloModale = $(this).data("titolo");
+    $("#title-modal").text(titoloModale);
+    let path = `https://openlibrary.org${key}.json`;
+    $.ajax({
+      url: path,
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        let description = data.description.value;
+        $("#modale-description").text(description);
       },
       error: function (error) {
         console.error("Errore durante la richiesta:", error);
